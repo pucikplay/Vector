2a791ab28cbc9ec83726583c6bd1cbbef84b018f
pucikplay <pucikplay@gmail.com>
2022-05-02 13:35:05 +0200

    Small test implemented

--- test/test.c
+++ test/test.c
@@ -5,4 +5,24 @@
 int main(void)
 {
     printf("This is a tester program for vector.h\n");
+
+    size_t n;
+    scanf("%ld", &n);
+
+    int test_arr[n];
+
+    Vector* vec = vector_create();
+
+    for (size_t i = 0; i < n; i++)
+    {
+        test_arr[i] = i;
+        vector_append(vec, i);
+    }
+
+    for (size_t i = 0; i < n; i++)
+    {
+        printf("Vector: %d, test_arr: %d\n", vector_pop(vec), test_arr[n - 1 - i]);
+    }
+
+    vector_is_empty(vec) ? printf("true\n") : printf("false\n");
 }
